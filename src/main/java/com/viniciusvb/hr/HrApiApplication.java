6055f0fb4f7f1bc8300d3ce58f34089e0a4c69eb
viniciusvilasbs <vinyvilas@gmail.com>
2023-07-24 15:38:26 -0300

    Project created

---
+++ src/main/java/com/viniciusvb/hr/HrApiApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusvb.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusvb/hr/HrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusvb.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
